205bc7534f8130b8401746da7083700250c90cfb
j2noo <wlsdndml213@naver.com>
2023-08-03 18:20:50 +0900

    docs : React 기본 구조 설정

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const add = (a: number, b: number): number => a + b;
+const App = () => {
+  return <div className="container">Rsdsdeact!</div>;
+};
+
+export default App;
+                         
