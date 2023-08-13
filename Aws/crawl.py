ecf45b831ae7c6c8645ef395dda5164a9ba2709b
j2noo <wlsdndml213@naver.com>
2023-08-14 01:17:42 +0900

    test : 성능 테스트

--- Aws/crawl.py
+++ Aws/crawl.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.by import By  # 비동기 크롤링
 from selenium.webdriver.support.ui import WebDriverWait  # 비동기 크롤링
 from selenium.webdriver.support import expected_conditions as EC  # 비동기 크롤링
 
+from bs4 import BeautifulSoup # 파싱라입러리
+
 import time
 from datetime import datetime, timedelta  # 크롤링 시간 측정
 
@@ -18,18 +20,11 @@ chrome_options.add_experimental_option("detach", True)  # 꺼지지 않음
 driver = webdriver.Chrome("chromedriver.exe", options=chrome_options)
 
 
-test_urls = [
-    "https://m-flight.naver.com/flights/international/ICN-DAD-20231001?adult=1&isDirect=true&fareType=Y",
-    "https://m-flight.naver.com/flights/international/ICN-DAD-20231002?adult=1&isDirect=true&fareType=Y",
-    "https://m-flight.naver.com/flights/international/ICN-DAD-20231003?adult=1&isDirect=true&fareType=Y",
-    "https://m-flight.naver.com/flights/international/ICN-DAD-20231004?adult=1&isDirect=true&fareType=Y",
-    "https://m-flight.naver.com/flights/international/ICN-DAD-20231005?adult=1&isDirect=true&fareType=Y",
-    "https://m-flight.naver.com/flights/international/ICN-DAD-20231006?adult=1&isDirect=true&fareType=Y",
-    "https://m-flight.naver.com/flights/international/ICN-DAD-20231007?adult=1&isDirect=true&fareType=Y",
-    "https://m-flight.naver.com/flights/international/ICN-DAD-20231008?adult=1&isDirect=true&fareType=Y",
-]
 
-for url in test_urls:
+
+for date in range(20231001, 20231005):  # 총 30일
+    url = f"https://m-flight.naver.com/flights/international/ICN-DAD-{date}?adult=1&isDirect=true&fareType=Y"
+
     startTime = datetime.today()
     driver.get(url)
     try:
@@ -65,3 +60,19 @@ for url in test_urls:
         By.XPATH, '//*[@id="__next"]/div/div[1]/div[6]/div/div[1]/div/div[2]/div/button[2]'
     ).click()
     time.sleep(1)
+
+    # selenium을 이용한 parsing ---------------------------------------------------------------------------------------
+    before_se = datetime.today()
+    crawledhtml = driver.find_elements(By.CSS_SELECTOR, "div.indivisual_IndivisualItem__3co62.result")
+    after_se = datetime.today()
+    print(f" crawl-selen : {after_se-before_se}(ms) cnt : {len(crawledhtml)} ")
+
+    # bs4를 이용한 parsing=======================================================
+    before_bs4 = datetime.today()
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    crawledHtml_li = soup.find_all("div", class_="indivisual_IndivisualItem__3co62 result")
+    after_bs4 = datetime.today()
+    print(f"crawl-bs4..: {after_bs4-before_bs4}(ms) cnt : {len(crawledHtml_li)} ")
+
+print("평균 소요 시간")
+print(f"selenium :")
