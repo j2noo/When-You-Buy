bfdbec484a98f5f9bf6bf549079bcbbe0273f668
j2noo <wlsdndml213@naver.com>
2023-08-14 15:17:43 +0900

    refactor : post 분리

--- Aws/api-crawl.py
+++ Aws/api-crawl.py
@@ -1,7 +1,6 @@
 import json  # json 파싱용
 import time
 from datetime import datetime, timedelta  # 크롤링 시간 측정
-
 import requests  # http 통신
 
 routes = [
@@ -10,83 +9,94 @@ routes = [
 ]
 
 startTime = datetime.today()
+
+
+def getResponseJson(departureAirport, arrivalAirport, departureDate):
+    url = "https://airline-api.naver.com/graphql"
+    headers = {
+        "Content-Type": "application/json",
+        "Referer": f"https://m-flight.naver.com/flights/international/{departureAirport}-{arrivalAirport}-{departureDate}?adult=1&isDirect=true&fareType=Y",
+    }
+
+    # GraphQL 요청 본문 데이터 정의
+    first_payload = {
+        "operationName": "getInternationalList",
+        "variables": {
+            "adult": 1,
+            "child": 0,
+            "infant": 0,
+            "where": "pc",
+            "isDirect": True,
+            "galileoFlag": True,
+            "travelBizFlag": True,
+            "fareType": "Y",
+            "itinerary": [
+                {
+                    "departureAirport": departureAirport,
+                    "arrivalAirport": arrivalAirport,
+                    "departureDate": departureDate,
+                }
+            ],
+            "stayLength": "",
+            "trip": "OW",
+            "galileoKey": "",
+            "travelBizKey": "",
+        },
+        "query": 'query getInternationalList($trip: String!, $itinerary: [InternationalList_itinerary]!, $adult: Int = 1, $child: Int = 0, $infant: Int = 0, $fareType: String!, $where: String = "pc", $isDirect: Boolean = false, $stayLength: String, $galileoKey: String, $galileoFlag: Boolean = true, $travelBizKey: String, $travelBizFlag: Boolean = true) {\n  internationalList(\n    input: {trip: $trip, itinerary: $itinerary, person: {adult: $adult, child: $child, infant: $infant}, fareType: $fareType, where: $where, isDirect: $isDirect, stayLength: $stayLength, galileoKey: $galileoKey, galileoFlag: $galileoFlag, travelBizKey: $travelBizKey, travelBizFlag: $travelBizFlag}\n  ) {\n    galileoKey\n    galileoFlag\n    travelBizKey\n    travelBizFlag\n    totalResCnt\n    resCnt\n    results {\n      airlines\n      airports\n      fareTypes\n      schedules\n      fares\n      errors\n    }\n  }\n}\n',
+    }
+
+    first_response = requests.post(url, json=first_payload, headers=headers)
+    first_response_json = first_response.json()
+
+    travel_biz_key = first_response_json["data"]["internationalList"]["travelBizKey"]
+    galileo_key = first_response_json["data"]["internationalList"]["galileoKey"]
+
+    print(f"{departureAirport} to {arrivalAirport} at {departureDate}========")
+    print("travel key: ", travel_biz_key)
+    print("galileo key: ", galileo_key)
+
+    time.sleep(5)
+    second_payload = {
+        "operationName": "getInternationalList",
+        "variables": {
+            "adult": 1,
+            "child": 0,
+            "infant": 0,
+            "where": "pc",
+            "isDirect": True,
+            "galileoFlag": galileo_key != "",
+            "travelBizFlag": travel_biz_key != "",  # 값이 없으면 false
+            "fareType": "Y",
+            "itinerary": [
+                {
+                    "departureAirport": departureAirport,
+                    "arrivalAirport": arrivalAirport,
+                    "departureDate": departureDate,
+                }
+            ],
+            "stayLength": "",
+            "trip": "OW",
+            "galileoKey": galileo_key,
+            "travelBizKey": travel_biz_key,
+        },
+        "query": 'query getInternationalList($trip: String!, $itinerary: [InternationalList_itinerary]!, $adult: Int = 1, $child: Int = 0, $infant: Int = 0, $fareType: String!, $where: String = "pc", $isDirect: Boolean = false, $stayLength: String, $galileoKey: String, $galileoFlag: Boolean = true, $travelBizKey: String, $travelBizFlag: Boolean = true) {\n  internationalList(\n    input: {trip: $trip, itinerary: $itinerary, person: {adult: $adult, child: $child, infant: $infant}, fareType: $fareType, where: $where, isDirect: $isDirect, stayLength: $stayLength, galileoKey: $galileoKey, galileoFlag: $galileoFlag, travelBizKey: $travelBizKey, travelBizFlag: $travelBizFlag}\n  ) {\n    galileoKey\n    galileoFlag\n    travelBizKey\n    travelBizFlag\n    totalResCnt\n    resCnt\n    results {\n      airlines\n      airports\n      fareTypes\n      schedules\n      fares\n      errors\n    }\n  }\n}\n',
+    }
+
+    # GraphQL POST 요청 보내기
+    second_response = requests.post(url, json=second_payload, headers=headers)  # 가져올때도있고 아닐때도 있고. 비동기로 처리?
+    second_response_json = second_response.json()
+
+    return second_response_json
+
+
 # 10일 이후까지
 for route in routes:
     for days in range(30, 33):
         departureDate = (startTime + timedelta(days=days)).strftime("%Y%m%d")
-        print(departureDate)
 
-        url = "https://airline-api.naver.com/graphql"
-        headers = {
-            "Content-Type": "application/json",
-            "Referer": f"https://m-flight.naver.com/flights/international/{route[0]}-{route[1]}-{departureDate}?adult=1&isDirect=true&fareType=Y",
-        }
-        # GraphQL 요청 본문 데이터 정의
+        response_json = getResponseJson(route[0], route[1], departureDate)
 
-        first_payload = {
-            "operationName": "getInternationalList",
-            "variables": {
-                "adult": 1,
-                "child": 0,
-                "infant": 0,
-                "where": "pc",
-                "isDirect": True,
-                "galileoFlag": True,
-                "travelBizFlag": True,
-                "fareType": "Y",
-                "itinerary": [
-                    {"departureAirport": route[0], "arrivalAirport": route[1], "departureDate": departureDate}
-                ],
-                "stayLength": "",
-                "trip": "OW",
-                "galileoKey": "",
-                "travelBizKey": "",
-            },
-            "query": 'query getInternationalList($trip: String!, $itinerary: [InternationalList_itinerary]!, $adult: Int = 1, $child: Int = 0, $infant: Int = 0, $fareType: String!, $where: String = "pc", $isDirect: Boolean = false, $stayLength: String, $galileoKey: String, $galileoFlag: Boolean = true, $travelBizKey: String, $travelBizFlag: Boolean = true) {\n  internationalList(\n    input: {trip: $trip, itinerary: $itinerary, person: {adult: $adult, child: $child, infant: $infant}, fareType: $fareType, where: $where, isDirect: $isDirect, stayLength: $stayLength, galileoKey: $galileoKey, galileoFlag: $galileoFlag, travelBizKey: $travelBizKey, travelBizFlag: $travelBizFlag}\n  ) {\n    galileoKey\n    galileoFlag\n    travelBizKey\n    travelBizFlag\n    totalResCnt\n    resCnt\n    results {\n      airlines\n      airports\n      fareTypes\n      schedules\n      fares\n      errors\n    }\n  }\n}\n',
-        }
-
-        first_response = requests.post(url, json=first_payload, headers=headers)
-        first_response_json = first_response.json()
-        schedules = first_response_json["data"]["internationalList"]["results"]["schedules"]
-        travel_biz_key = first_response_json["data"]["internationalList"]["travelBizKey"]
-        galileo_key = first_response_json["data"]["internationalList"]["galileoKey"]
-
-        # print(schedules)
-        print(f"---{departureDate}---")
-        print("travel key: ", travel_biz_key)
-        print("galileo key: ", galileo_key)
-        # print(response_data)
-
-        time.sleep(5)
-        second_payload = {
-            "operationName": "getInternationalList",
-            "variables": {
-                "adult": 1,
-                "child": 0,
-                "infant": 0,
-                "where": "pc",
-                "isDirect": True,
-                "galileoFlag": galileo_key != "",
-                "travelBizFlag": travel_biz_key != "",  # 값이 없으면 false
-                "fareType": "Y",
-                "itinerary": [
-                    {"departureAirport": route[0], "arrivalAirport": route[1], "departureDate": departureDate}
-                ],
-                "stayLength": "",
-                "trip": "OW",
-                "galileoKey": galileo_key,
-                "travelBizKey": travel_biz_key,
-            },
-            "query": 'query getInternationalList($trip: String!, $itinerary: [InternationalList_itinerary]!, $adult: Int = 1, $child: Int = 0, $infant: Int = 0, $fareType: String!, $where: String = "pc", $isDirect: Boolean = false, $stayLength: String, $galileoKey: String, $galileoFlag: Boolean = true, $travelBizKey: String, $travelBizFlag: Boolean = true) {\n  internationalList(\n    input: {trip: $trip, itinerary: $itinerary, person: {adult: $adult, child: $child, infant: $infant}, fareType: $fareType, where: $where, isDirect: $isDirect, stayLength: $stayLength, galileoKey: $galileoKey, galileoFlag: $galileoFlag, travelBizKey: $travelBizKey, travelBizFlag: $travelBizFlag}\n  ) {\n    galileoKey\n    galileoFlag\n    travelBizKey\n    travelBizFlag\n    totalResCnt\n    resCnt\n    results {\n      airlines\n      airports\n      fareTypes\n      schedules\n      fares\n      errors\n    }\n  }\n}\n',
-        }
-
-        # GraphQL POST 요청 보내기
-
-        second_response = requests.post(
-            url, json=second_payload, headers=headers
-        )  # 가져올때도있고 아닐때도 있고. 비동기로 처리?
-        second_response_json = second_response.json()
-        print(json.dumps(second_response_json, indent=4))
+        print(json.dumps(response_json, indent=4))
 
         errs = second_response_json["data"]["internationalList"]["results"]["errors"]
         # print("err : ", errs)
