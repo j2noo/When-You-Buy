ca1187105a3a69b152cbba60db7be455a003c026
j2noo <wlsdndml213@naver.com>
2023-08-31 22:18:39 +0900

    feat : GlobalStyle, GlobalFont 설정

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -3,10 +3,14 @@ import Header from "./Components/Header";
 import Nav from "./Components/Nav";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
+import GlobalStyle from "./styles/GlobalStyle";
+import GlobalFont from "./styles/GlobalFont";
 const add = (a: number, b: number): number => a + b;
 const App = () => {
   return (
     <>
+      <GlobalStyle></GlobalStyle>
+      <GlobalFont></GlobalFont>
       <Header></Header>
       <Nav></Nav>
       <Main></Main>
